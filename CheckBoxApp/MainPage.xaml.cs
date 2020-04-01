e1f0470b0a94ed3709a99315f2001bd86da8c8e8
Fay Wang <v-faywan@microsoft.com>
2020-04-01 10:29:53 +0800

    Add project files.

---
+++ CheckBoxApp/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.Capture.Frames;
+using Windows.Media.MediaProperties;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CheckBoxApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            //Method2
+            if (Checkbox1.IsChecked == true)
+            {
+                MyTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else if (Checkbox1.IsChecked == false)
+            {
+                MyTextBlock.Foreground = new SolidColorBrush(Colors.Yellow);
+            }
+            
+        }
+    }
+}
